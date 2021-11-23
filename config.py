a4bbe80b113839a60f2a8358cb0fd47b5a2ae3b8
Eduardo Graminho <edugraminho@gmail.com>
2021-11-23 16:59:47 -0300

    config.py to select specific Agency

--- config.py
+++ config.py
@@ -1,32 +1,42 @@
+from pathlib import Path
+import os
 
+ROOT = Path(os.path.dirname(os.path.abspath(__file__)))
+DOWNLOADS_PATH = os.path.join(ROOT, 'output')
+
+# ---------------------- Select a Departament -------------------------
 
 AGENCY = "Department of Education"
 
+"""
+Department of Agriculture
+Department of Commerce
+Department of Defense
+Department of Health and Human Services
+Department of the Interior
+Department of Justice
+Department of Labor
+Department of State
+Department of the Treasury
+Social Security Administration
+Department of Education
+Department of Energy
+Environmental Protection Agency
+Department of Transportation
+General Services Administration
+Department of Homeland Security
+Department of Housing and Urban Development
+National Aeronautics and Space Administration
+Office of Personnel Management
+Small Business Administration
+Department of Veterans Affairs
+U.S. Agency for International Development
+U.S. Army Corps of Engineers
+National Archives and Records Administration
+National Science Foundation
+Nuclear Regulatory Commission
+"""
+# -----------------------------------------------------------------------
 
 
-# Department of Agriculture
-# Department of Commerce
-# Department of Defense
-# Department of Health and Human Services
-# Department of the Interior
-# Department of Justice
-# Department of Labor
-# Department of State
-# Department of the Treasury
-# Social Security Administration
-# Department of Education
-# Department of Energy
-# Environmental Protection Agency
-# Department of Transportation
-# General Services Administration
-# Department of Homeland Security
-# Department of Housing and Urban Development
-# National Aeronautics and Space Administration
-# Office of Personnel Management
-# Small Business Administration
-# Department of Veterans Affairs
-# U.S. Agency for International Development
-# U.S. Army Corps of Engineers
-# National Archives and Records Administration
-# National Science Foundation
-# Nuclear Regulatory Commission
+
